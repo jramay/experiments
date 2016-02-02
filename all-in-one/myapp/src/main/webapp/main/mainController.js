d5412f2088ee976af9e79054752495af3ef9781d
jramay <jaffar.ramay@gmail.com>
2016-02-01 21:17:02 -0600

    Added Log service

--- all-in-one/myapp/src/main/webapp/main/mainController.js
+++ all-in-one/myapp/src/main/webapp/main/mainController.js
@@ -1,12 +1,11 @@
-mainApp.controller('mainController', function($scope, dataService) {
-	
-	$scope.getMessage = function() {		
-		dataService.getMessage($scope.name).
-	    success(function(data) {
-	      $scope.message = data.message;
-	    }).
-	    error(function(data, status, headers, config) {
-	      // log error
-	    });
-	  };
+mainApp.controller('mainController', function($scope, $log, dataService) {
+
+	$scope.getMessage = function() {
+		dataService.getMessage($scope.name).success(function(data) {
+			$scope.message = data.message;
+			$log.info('Got Response ' + JSON.stringify(data));
+		}).error(function(data, status, headers, config) {
+			$log.error('Got Error');
+		});
+	};
 });
