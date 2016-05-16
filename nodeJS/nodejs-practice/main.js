1d2a5b477f366bebf6ab33e453dc9cea06dee57e
Muhammad Ramay <muhammad@Muhammads-MacBook-Pro.local>
2016-05-16 15:40:20 -0500

    Docker Commit

--- nodeJS/nodejs-practice/main.js
+++ nodeJS/nodejs-practice/main.js
@@ -5,16 +5,22 @@ console.log("NodeJS Practice Project on EC2 branch");
 
 var http = require("http");
 
-http.createServer(function (request, response) {
+http.createServer(function(request, response) {
 
-	   // Send the HTTP header 
-	   // HTTP Status: 200 : OK
-	   // Content Type: text/plain
-	   response.writeHead(200, {'Content-Type': 'text/plain'});
-	   
-	   // Send the response body as "Hello World"
-	   response.end('Work in Progress by Jaffar Ramay\n');
-	}).listen(8081);
+	// Send the HTTP header 
+	// HTTP Status: 200 : OK
+	// Content Type: text/plain
+	response.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+jaffar();
+	// Send the response body as "Hello World"
+	response.end('Work in Progress by Jaffar Ramay for Docker on AWS\n');
+}).listen(8081);
 
-	// Console will print the message
-	console.log('Server running at http://127.0.0.1:8081/');
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
+function jaffar(){
+
+}
